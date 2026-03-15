edf9b45270b844a37eba3b7e4c2ee42e002a82f9
Sana Deghat <Sana.Deghat@gmail.com>
2026-03-15 01:05:58 -0700

    make windows draggble

--- script.js
+++ script.js
@@ -1,3 +1,45 @@
 setInterval(function () {
   document.querySelector("#timeElement").innerHTML = new Date().toLocaleString();
 }, 1000);
+
+const allWindows = document.querySelectorAll(".window");
+
+allWindows.forEach((win) => {
+    dragElement(win);
+});
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    var header = elmnt.querySelector(".window-header");
+
+  if (header) {
+    header.onmousedown = dragMouseDown;
+  } else { 
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = stopDragging;
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function stopDragging() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
